1a11dadb98b02d3ef3c85479971bba8c16ac41cb
DCoelho <coelho.diogogoncalves@gmail.com>
2024-12-06 13:43:09 +0000

    Full screen, Q to quid

--- settings.py
+++ settings.py
@@ -5,11 +5,11 @@ class Settings:
         """Initialize game's settings """
 
         # Screen Settings
-        self.screen_width = 1200
+        self.screen_width = 600
         self.screen_height = 800
         self.bg_color = (103, 181, 246)
 
         # Ship Settings
-        self.ship_speed = 1.5
+        self.ship_speed = 10000000.5
 
         
--- ship.py
+++ ship.py
@@ -6,8 +6,8 @@ class Ship:
     def __init__(self, ai_game):
         """ Initialize the ship and set its starting position """
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
 
         #Load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
@@ -26,10 +26,10 @@ class Ship:
     def update(self):
         """ Update the ship's position based on movement flags."""
         # Update the ship's x value, not the rect.
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.rect.x += self.settings.ship_speed
 
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.rect.x -= self.settings.ship_speed
 
         # Update rect object from self.x
